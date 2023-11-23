9b8ba42b18df197e085baa1454ba400499955576
kvtemadden <katy.madden99@gmail.com>
2023-11-23 15:51:03 +0000

    Add hover effect and job description to JobCard component

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,10 +1,14 @@
+import React, { useState } from "react";
 import clsx from "clsx";
+import Link from "next/link";
 
 interface JobCardProps {
   className?: string;
 }
 
 export const JobCard: React.FC<JobCardProps> = ({ className }) => {
+  const [hoveredJob, setHoveredJob] = useState<string | null>(null);
+
   const jobs = [
     {
       title: "AdBuilder AI",
@@ -23,21 +27,35 @@ export const JobCard: React.FC<JobCardProps> = ({ className }) => {
       image: "bg-ABAG",
     },
   ];
+
   return (
     <div className={clsx("flex flew-row gap-4", className)}>
       {jobs.map((job) => (
-        <div className="relative w-96 h-96 border-4" key={job.title}>
+        <div
+          className={clsx("relative w-96 h-96 border-4", {
+            "hovered-job": job.title === hoveredJob,
+          })}
+          key={job.title}
+          onMouseEnter={() => setHoveredJob(job.title)}
+          onMouseLeave={() => setHoveredJob(null)}
+        >
           <div
             className={clsx(
               "bg-cover top-0 left-0 w-full h-full bg-right",
               job.image
             )}
           ></div>
-          <div className="absolute top-0 left-0 w-full h-full bg-zinc-900/80 flex justify-center items-center">
-            <div className="flex flex-col">
-              <h3 className="text-xl font-bold">{job.title}</h3>
-              <h4 className="text-sm uppercase">{job.company}</h4>
-            </div>
+          <div className="absolute top-0 left-0 w-full h-full bg-slate-900/90 flex justify-center items-center flex-col">
+            {job.title === hoveredJob ? (
+              <>
+                <p className="text-white p-5">{job.description}</p>
+              </>
+            ) : (
+              <>
+                <h3 className="text-xl font-bold">{job.title}</h3>
+                <h4 className="text-sm uppercase">{job.company}</h4>
+              </>
+            )}
           </div>
         </div>
       ))}
